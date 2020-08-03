501991f7abed85943f38e33d5a4f2e4b8cb483fe
ahmdhfdz <ahmad.hafidz98@yahoo.co.id>
2020-08-03 23:42:55 +0700

    axios init & api

---
+++ src/api/api.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+class api {
+    header={
+        headers:{}
+    }
+    
+    get(endPoint) {
+        return new Promise((resolve) => {
+            axios.get(endPoint, this.header)
+            .then((response) => {
+                resolve(response)
+            })
+            .catch((error) => {
+                this.catchError(error)
+            })
+        })
+    }
+
+    post(endPoint, data) {
+        return new Promise((resolve) => {
+            axios.post(endPoint, data, this.header)
+            .then((response) => {
+                resolve(response)
+            })
+            .catch((error) => {
+                this.catchError(error)
+            })
+        })
+    }
+
+}
+
+export default new api
